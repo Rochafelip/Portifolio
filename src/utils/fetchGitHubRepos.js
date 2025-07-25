05418eeed0d9b2e88ae96032ea6c19e2e8641558
Felipe <rocha.felipe98@gmail.com>
2025-07-25 15:36:56 -0300

    Update no token de verificação

--- src/utils/fetchGitHubRepos.js
+++ src/utils/fetchGitHubRepos.js
@@ -13,19 +13,11 @@ const getRepoImage = (name) => {
   return images[name] || placeHolderImg;
 };
 
-const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
-
-const headers = {
-  Authorization: `Bearer ${GITHUB_TOKEN}`
-};
-
 export const fetchGitHubRepos = async (username = "Rochafelip", limit = 10) => {
   const excludedRepos = ["Portifolio", "Rochafelip", "simulate-pit-stop"];
 
   try {
-    const response = await fetch(`https://api.github.com/users/${username}/repos`, {
-      headers
-    });
+    const response = await fetch(`https://api.github.com/users/${username}/repos`);
     const data = await response.json();
 
     if (!Array.isArray(data)) throw new Error('Resposta inesperada da API do GitHub');
@@ -45,9 +37,7 @@ export const fetchGitHubRepos = async (username = "Rochafelip", limit = 10) => {
 
     const reposWithLang = await Promise.all(
       repos.map(async (repo) => {
-        const langRes = await fetch(`https://api.github.com/repos/${username}/${repo.name}/languages`, {
-          headers
-        });
+        const langRes = await fetch(`https://api.github.com/repos/${username}/${repo.name}/languages`);
         const languagesData = await langRes.json();
         const languages = Object.keys(languagesData).join(', ') || "Não definidas";
 
